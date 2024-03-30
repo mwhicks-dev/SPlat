8830615dc34621c2d2e763bccd8a27a23b266c29
Mason Hicks <mason@hicksm.dev>
2024-03-30 10:31:27 -0400

    GOM now uses references of abstract class Asset #1

--- include/GameObjectModel.h
+++ include/GameObjectModel.h
@@ -5,6 +5,14 @@
 
 #include "Asset.h"
 
+/// NOTE
+/// Even though references are passed and stored, all memory management is to 
+/// be handled internally by the GOM class. No Asset should ever be created by 
+/// a SPlat user in the heap; instead, stack should be passed, which will then 
+/// be totally managed by our GOM.
+
+/// I may make the GOM a singleton so that it is easier to deconstruct.
+
 namespace SPlat {
 
     class GameObjectModel {
@@ -15,13 +23,13 @@ namespace SPlat {
 
     public:
 
-        static Asset create_asset(Asset);
+        static Asset& create_asset(Asset&);
 
-        static Asset read_asset(Asset);
+        static Asset& read_asset(Asset&);
 
-        static Asset update_asset(size_t, Asset);
+        static Asset& update_asset(size_t, Asset&);
 
-        static Asset delete_asset(size_t);
+        static Asset& delete_asset(size_t);
 
     };
 
