72d30f7d7da5406bc17cd82c1ca12e5a59517efe
Mason Hicks <mason@hicksm.dev>
2024-03-30 10:53:11 -0400

    Implemented GOM #1

---
+++ src/GameObjectModel.cpp
@@ -0,0 +1,72 @@
+#include "GameObjectModel.h"
+
+namespace SPlat {
+
+    Asset& GameObjectModel::create_asset(Asset& asset) {
+        lock.lock();
+
+        // Get Asset ID
+        size_t id = assets.size();
+        while (assets.count(id) > 0) id++;
+
+        // Assign ID and store
+        asset.id = id;
+        assets[id] = &asset;
+
+        lock.unlock();
+
+        return asset;
+    }
+
+    Asset& GameObjectModel::read_asset(size_t id) {
+        lock.lock();
+
+        if (assets.count(id) == 0) {
+            lock.unlock();
+            throw std::invalid_argument("No asset with id " + id);
+        }
+        
+        Asset& asset = *assets[id];
+
+        lock.unlock();
+
+        return asset;
+    }
+
+    Asset& GameObjectModel::update_asset(size_t id, Asset& update) {
+        lock.lock();
+
+        if (assets.count(id) == 0) {
+            lock.unlock();
+            throw std::invalid_argument("No asset with id " + id);
+        }
+
+        Asset& asset = *assets[id];
+
+        asset.setPosition(update.getPosition());
+        asset.setTexture(update.getTexture());
+        asset.setTextureRect(update.getTextureRect());
+
+        lock.unlock();
+
+        return asset;
+    }
+
+    Asset& GameObjectModel::delete_asset(size_t id) {
+        lock.lock();
+
+        if (assets.count(id) == 0) {
+            lock.unlock();
+            throw std::invalid_argument("No asset with id " + id);
+        }
+
+        Asset& asset = *assets[id];
+
+        assets.erase(id);
+
+        lock.unlock();
+
+        return asset;
+    }
+
+}
