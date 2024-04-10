a4eaef85d03dce1a4b135ada695dabfc40ffee13
Mason Hicks <mason@hicksm.dev>
2024-04-10 13:04:33 -0400

    unit tests now use SPlat namespace

--- test/TestEvents.cpp
+++ test/TestEvents.cpp
@@ -2,45 +2,47 @@
 #include <iostream>
 #include "events/KeyEvents.h"
 
+using namespace SPlat;
+
 TEST(EventTest, KeyEvents) {
-    SPlat::Events::Event::handlers[SPlat::Events::KeyPressEvent::KEY_PRESS_EVENT_TAG] = SPlat::Events::KeyPressEvent::handler;
-    SPlat::Events::Event::handlers[SPlat::Events::KeyReleaseEvent::KEY_RELEASE_EVENT_TAG] = SPlat::Events::KeyReleaseEvent::handler;
+    Events::Event::handlers[Events::KeyPressEvent::KEY_PRESS_EVENT_TAG] = Events::KeyPressEvent::handler;
+    Events::Event::handlers[Events::KeyReleaseEvent::KEY_RELEASE_EVENT_TAG] = Events::KeyReleaseEvent::handler;
 
     // Pick arbitrary key -- should pass for any
     sf::Keyboard::Key k(sf::Keyboard::Key::Q);
 
     // Assert that key is not held
-    ASSERT_FALSE(SPlat::Events::KeyEvent::is_key_held(k));
+    ASSERT_FALSE(Events::KeyEvent::is_key_held(k));
 
     // Create and (manually) dispatch a key press event
-    SPlat::Events::KeyPressEvent press(k);
+    Events::KeyPressEvent press(k);
     press.dispatch();
 
     // Assert that key is now held
-    ASSERT_TRUE(SPlat::Events::KeyEvent::is_key_held(k));
+    ASSERT_TRUE(Events::KeyEvent::is_key_held(k));
 
     // Create and (manually) dispatch a key release event
-    SPlat::Events::KeyReleaseEvent release(k);
+    Events::KeyReleaseEvent release(k);
     release.dispatch();
 
     // Assert that key is not held
-    ASSERT_FALSE(SPlat::Events::KeyEvent::is_key_held(k));
+    ASSERT_FALSE(Events::KeyEvent::is_key_held(k));
 
     // Double dispatch key press
     press.dispatch(); press.dispatch();
 
     // Assert that key is now held
-    ASSERT_TRUE(SPlat::Events::KeyEvent::is_key_held(k));
+    ASSERT_TRUE(Events::KeyEvent::is_key_held(k));
 
     // Dispatch key release
     release.dispatch();
 
     // Assert that key is not held
-    ASSERT_FALSE(SPlat::Events::KeyEvent::is_key_held(k));
+    ASSERT_FALSE(Events::KeyEvent::is_key_held(k));
 
     // Dispatch key release again
     release.dispatch();
 
     // Assert that key is not held
-    ASSERT_FALSE(SPlat::Events::KeyEvent::is_key_held(k));
+    ASSERT_FALSE(Events::KeyEvent::is_key_held(k));
 }
--- test/TestGameObjectModel.cpp
+++ test/TestGameObjectModel.cpp
@@ -5,9 +5,11 @@
 #include "model/MovingPlatform.h"
 #include "model/GameObjectModel.h"
 
+using namespace SPlat;
+
 TEST(GOMTest, Acquisition) {
     try {
-        SPlat::Model::GameObjectModel& gom = SPlat::Model::GameObjectModel::get_instance();
+        Model::GameObjectModel& gom = Model::GameObjectModel::get_instance();
         ASSERT_TRUE(true);
     } catch (std::exception& e) {
         FAIL();
@@ -17,30 +19,30 @@ TEST(GOMTest, Acquisition) {
 TEST(GOMTest, TestAsset) {
     sf::Vector2f size(50, 100);
 
-    SPlat::Model::Character base1(size);
-    SPlat::Model::Asset& asset1(base1);
+    Model::Character base1(size);
+    Model::Asset& asset1(base1);
     ASSERT_EQ(0, asset1.get_priority());
 
-    SPlat::Model::Platform base2(size);
-    SPlat::Model::Asset& asset2(base2);
+    Model::Platform base2(size);
+    Model::Asset& asset2(base2);
     ASSERT_EQ(-2, asset2.get_priority());
 
-    SPlat::Model::MovingPlatform base3(size);
-    SPlat::Model::Asset& asset3(base3);
+    Model::MovingPlatform base3(size);
+    Model::Asset& asset3(base3);
     ASSERT_EQ(-1, asset3.get_priority());
 }
 
 TEST(GOMTest, TestGOM) {
     sf::Vector2f size(50, 100);
-    SPlat::Model::Character &base = *new SPlat::Model::Character(size);
+    Model::Character &base = *new Model::Character(size);
 
-    SPlat::Model::Asset& character = SPlat::Model::GameObjectModel::get_instance().create_asset(base);
+    Model::Asset& character = Model::GameObjectModel::get_instance().create_asset(base);
     ASSERT_EQ(character.id, 0);
     ASSERT_EQ(character.getPosition(), sf::Vector2f(0, 0));
     ASSERT_EQ(character.getSize(), sf::Vector2f(50, 100));
 
-    SPlat::Model::Character base2 = *new SPlat::Model::Character(size);
-    SPlat::Model::Asset& character2 = SPlat::Model::GameObjectModel::get_instance().create_asset(base2);
+    Model::Character base2 = *new Model::Character(size);
+    Model::Asset& character2 = Model::GameObjectModel::get_instance().create_asset(base2);
     ASSERT_EQ(character.id, 0);
     ASSERT_EQ(character.getPosition(), sf::Vector2f(0, 0));
     ASSERT_EQ(character.getSize(), sf::Vector2f(50, 100));
@@ -49,12 +51,12 @@ TEST(GOMTest, TestGOM) {
     ASSERT_EQ(character2.getSize(), sf::Vector2f(50, 100));
 
     size += sf::Vector2f(100, 100);
-    SPlat::Model::Character updated(size);
+    Model::Character updated(size);
     updated.setPosition(100, 100);
 
-    SPlat::Model::GameObjectModel::get_instance().update_asset(character.id, updated);
+    Model::GameObjectModel::get_instance().update_asset(character.id, updated);
 
-    character = SPlat::Model::GameObjectModel::get_instance().read_asset(character.id);
+    character = Model::GameObjectModel::get_instance().read_asset(character.id);
 
     ASSERT_EQ(character.id, 0);
     ASSERT_EQ(character.getPosition(), sf::Vector2f(100, 100));
@@ -63,9 +65,9 @@ TEST(GOMTest, TestGOM) {
     ASSERT_EQ(character2.getPosition(), sf::Vector2f(0, 0));
     ASSERT_EQ(character2.getSize(), sf::Vector2f(50, 100));
 
-    SPlat::Model::GameObjectModel::get_instance().delete_asset(character.id);
+    Model::GameObjectModel::get_instance().delete_asset(character.id);
     try {
-        character2 = SPlat::Model::GameObjectModel::get_instance().read_asset(character.id);
+        character2 = Model::GameObjectModel::get_instance().read_asset(character.id);
         FAIL();
     } catch (std::exception& e) {
         ASSERT_TRUE(true);
@@ -75,8 +77,8 @@ TEST(GOMTest, TestGOM) {
     ASSERT_EQ(character2.getPosition(), sf::Vector2f(0, 0));
     ASSERT_EQ(character2.getSize(), sf::Vector2f(50, 100));
 
-    SPlat::Model::Character &base3 = *new SPlat::Model::Character(size);
-    SPlat::Model::Asset& character3 = SPlat::Model::GameObjectModel::get_instance().create_asset(base3);
+    Model::Character &base3 = *new Model::Character(size);
+    Model::Asset& character3 = Model::GameObjectModel::get_instance().create_asset(base3);
     ASSERT_EQ(character2.id, 1);
     ASSERT_EQ(character2.getPosition(), sf::Vector2f(0, 0));
     ASSERT_EQ(character2.getSize(), sf::Vector2f(50, 100));
