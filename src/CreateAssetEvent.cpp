608bb78f1f5097c67868e28ce9364af5aa89c6e9
Mason Hicks <mason@hicksm.dev>
2024-04-11 13:26:53 -0400

    Duplicate coded events now use shared function, close #4

--- src/CreateAssetEvent.cpp
+++ src/CreateAssetEvent.cpp
@@ -1,8 +1,5 @@
 #include "events/CreateAssetEvent.h"
-#include "model/Character.h"
-#include "model/Platform.h"
-#include "model/MovingPlatform.h"
-#include "model/GameObjectModel.h"
+#include "utilities/Functions.h"
 
 using namespace SPlat::Events;
 
@@ -12,26 +9,6 @@ std::string CreateAssetEvent::TYPE = "create_asset";
 /// @param serialized arguments passed as serialized string
 /// @throws if args cannot be deserialized or asset type invalid
 void CreateAssetEvent::handler(std::string serialized) {
-    // deserialize args from string
-    CreateAssetEventArgs args;
-    std::stringstream ss; ss << serialized;
-    {
-        cereal::JSONInputArchive iar(ss);
-        iar(args);
-    }
-
     // create new asset using GOM
-    SPlat::Model::Asset * created_asset;
-    if (args.type == SPlat::Model::Character::TYPE) {
-        created_asset = new SPlat::Model::Character(args.size);
-    } else if (args.type == SPlat::Model::Platform::TYPE) {
-        created_asset = new SPlat::Model::Platform(args.size);
-    } else if (args.type == SPlat::Model::MovingPlatform::TYPE) {
-        created_asset = new SPlat::Model::MovingPlatform(args.size);
-    } else {
-        throw std::domain_error("No such asset type " + args.type);
-    }
-
-    created_asset->setPosition(args.position);
-    SPlat::Model::GameObjectModel::get_instance().create_asset(*created_asset);
+    SPlat::Utilities::deserialize_asset(serialized);
 }
