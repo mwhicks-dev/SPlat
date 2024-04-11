a590b899e5f01736520e324178364ef535431ee8
Mason Hicks <mason@hicksm.dev>
2024-04-11 13:14:35 -0400

    Added cli func to handle key presses for testing #3

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,24 +1,38 @@
 #include "Client.h"
 #include "Controller.h"
 
+#include "events/KeyEvents.h"
+
 #include <thread>
+#include <iostream>
 
 using namespace SPlat;
 
 void Client::start() {
     window.create(sf::VideoMode(800, 600), "SPlat");
-    std::pair<bool, std::mutex> runtime;
+    std::pair<bool, std::mutex>& runtime = *new std::pair<bool, std::mutex>();
     runtime.first = true;
 
     Controller ctl;  // single controller
     std::thread t(&Controller::run, &ctl, std::ref(runtime));
 
     while (window.isOpen()) {
+        // poll for closed event
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) window.close();
         }
 
+        // check for keyboard inputs
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && !Events::KeyEvent::is_key_held(sf::Keyboard::Left)) {
+            ctl.push_event(Events::KeyPressEvent(sf::Keyboard::Left));
+            std::cout << "left key press" << std::endl;
+        } else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && Events::KeyEvent::is_key_held(sf::Keyboard::Left)) {
+            ctl.push_event(Events::KeyReleaseEvent(sf::Keyboard::Left));
+            std::cout << "left key release" << std::endl;
+        }
+
+        // draw all assets
         window.clear(sf::Color::Black);
 
         std::unordered_set<size_t> asset_ids = Model::GameObjectModel::
