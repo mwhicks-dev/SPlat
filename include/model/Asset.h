2af89d42518da2e7ceb58187d9aa93b3bd6aa280
Mason Hicks <mason@hicksm.dev>
2024-04-15 10:04:51 -0400

    Asset now has AssetProperties and get_type features #3

--- include/model/Asset.h
+++ include/model/Asset.h
@@ -3,9 +3,32 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "Serialization.h"
+
 namespace SPlat {
 
     namespace Model {
+
+        /// @brief communicable properties to use for Asset events
+        struct AssetProperties {
+
+            /// @brief string type of asset
+            std::string type;
+
+            /// @brief asset size vector
+            sf::Vector2f size;
+
+            /// @brief asset position vector
+            sf::Vector2f position;
+
+            /// @brief convert AssetProperties to serializable string
+            /// @tparam Archive cereal archive class
+            /// @param ar Archive to use for AssetProperties conversion
+            template <class Archive>
+            void serialize(Archive& ar) {
+                ar(type, size, position);
+            }
+        };
 
         class Asset : public sf::RectangleShape {
 
@@ -20,6 +43,12 @@ namespace SPlat {
             Asset(sf::Vector2f&);
 
             virtual int get_priority() = 0;
+
+            virtual std::string get_type() = 0;
+
+            AssetProperties get_properties() {
+                return {get_type(), getSize(), getPosition()};
+            }
 
         };
 
