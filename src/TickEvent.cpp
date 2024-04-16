d68d4bbcb76498c720e4fb9621b32efc70be9388
Mason Hicks <mason@hicksm.dev>
2024-04-16 14:07:55 -0400

    TickEvent now checks if asset standing on something before gravity #3

--- src/TickEvent.cpp
+++ src/TickEvent.cpp
@@ -27,7 +27,7 @@ void TickEvent::handler(std::string serialized) {
             // raise update asset event with velocity
             SPlat::Model::AssetProperties prop = curr.get_properties();
             prop.position += curr.velocity;
-            UpdateAssetEvent event = curr.get_priority() >= 0
+            UpdateAssetEvent event = curr.get_priority() >= 0 && curr.standing_on == nullptr
                 ? UpdateAssetEvent(curr.id, prop, sf::Vector2f(0, 1.5))
                 : UpdateAssetEvent(curr.id, prop);
             event.raise();
