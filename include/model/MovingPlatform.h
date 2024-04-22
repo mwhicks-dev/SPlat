82948a67b081b2e3acacdf7b4ec7508ad74caa6b
Mason Hicks <mason@hicksm.dev>
2024-04-22 11:09:14 -0400

    MovingPlatform header now contains State inner class #10

--- include/model/MovingPlatform.h
+++ include/model/MovingPlatform.h
@@ -9,7 +9,24 @@ namespace SPlat {
 
         class MovingPlatform : public Platform {
 
+            /// @brief "queue" of states to cycle between
+            std::vector<SPlat::Model::MovingPlatform::State> queue;
+
         public:
+
+            /// @brief MovingPlatform state; position, time and repeat
+            class State {
+
+                /// @brief state position
+                sf::Vector2f position;
+
+                /// @brief number of ticks from position until next position
+                size_t ticks_til_next;
+
+                /// @brief flag to add this state back to queue (true default) 
+                bool repeat = true;
+
+            };
 
             static std::string TYPE;
 
@@ -18,6 +35,10 @@ namespace SPlat {
             int get_priority() override;
 
             std::string get_type() override;
+
+            void add_state(State&);
+
+            void update() override;
 
         };
 
