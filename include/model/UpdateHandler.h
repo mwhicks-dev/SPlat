1c19ba7671c157a51434ee25ec42b8ac68addd86
Mason Hicks <mason@hicksm.dev>
2024-04-29 15:49:27 -0400

    Changed UpdateHandler to interface #23

--- include/model/UpdateHandler.h
+++ include/model/UpdateHandler.h
@@ -1,37 +1,15 @@
 #ifndef SPLAT_MODEL_UPDATEHANDLER_H
 #define SPLAT_MODEL_UPDATEHANDLER_H
 
-#include "model/MovingProperties.h"
-
 namespace SPlat {
 
     namespace Model {
 
         class UpdateHandler {
 
-            std::mutex m;
-
-            MovingProperties * properties = nullptr;
-
         public:
 
-            UpdateHandler() = default;
-
-            void set_properties(MovingProperties* properties) { 
-                m.lock();
-                this->properties = properties;
-                m.unlock();
-            }
-
-            MovingProperties * get_properties() {
-                m.lock();
-                MovingProperties * local = properties;
-                m.unlock();
-
-                return local;
-            }
-
-            virtual void update(time_t) = 0;
+            virtual void update() = 0;
 
         };
 
