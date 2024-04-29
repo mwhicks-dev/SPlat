89031dc617d6da53b0968252dafc776fe1d710a2
Mason Hicks <mason@hicksm.dev>
2024-04-29 16:00:12 -0400

    Asset abstraction contains CollisionHandler * with protected getters and setters #23

--- include/model/Asset.h
+++ include/model/Asset.h
@@ -1,7 +1,8 @@
 #ifndef SPLAT_MODEL_ASSET_H
 #define SPLAT_MODEL_ASSET_H
 
-#include "AssetProperties.h"
+#include "model/AssetProperties.h"
+#include "model/CollisionHandler.h"
 
 namespace SPlat {
 
@@ -12,6 +13,24 @@ namespace SPlat {
             std::mutex m;
 
             AssetProperties& properties;
+
+            CollisionHandler * collision_handler = nullptr;
+
+        protected:
+
+            void set_collision_handler(CollisionHandler * collision_handler) {
+                m.lock();
+                this->collision_handler = collision_handler;
+                m.unlock();
+            }
+
+            CollisionHandler * get_collision_handler() {
+                m.lock();
+                CollisionHandler * local = collision_handler;
+                m.unlock();
+
+                return local;
+            }
 
         public:
 
