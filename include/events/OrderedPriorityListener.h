6025919d284d1faebeff44edb098216258ec1f28
Mason Hicks <mason@hicksm.dev>
2024-04-30 13:57:58 -0400

    Created Listener implementation #31

---
+++ include/events/OrderedPriorityListener.h
@@ -0,0 +1,44 @@
+#ifndef SPlat_Events_OrderedPriorityListener_h
+#define SPlat_Events_OrderedPriorityListener_h
+
+#include "ListenerInterface.h"
+
+#include <queue>
+#include <map>
+#include <mutex>
+
+namespace SPlat {
+
+    namespace Events {
+
+        class OrderedPriorityListener : public ListenerInterface {
+
+            std::mutex m;
+
+            std::priority_queue<Command> command_queue;
+
+            std::map<std::string, CommandHandlerInterface&> handlers;
+
+            Command poll_command();
+
+            CommandHandlerInterface& get_handler(std::string);
+
+            bool command_available();
+
+            void listener_loop();
+
+        public:
+
+            void set_handler(std::string, CommandHandlerInterface&);
+
+            void push_command(Command);
+
+            void run();
+
+        };
+
+    }
+
+}
+
+#endif
---
+++ src/OrderedPriorityListener.cpp
@@ -0,0 +1,74 @@
+#include "events/OrderedPriorityListener.h"
+#include "Client.h"
+
+#include <thread>
+
+using namespace SPlat::Events;
+
+Command OrderedPriorityListener::poll_command() {
+    m.lock();
+    auto local = command_queue.top();
+    command_queue.pop();
+    m.unlock();
+
+    return local;
+}
+
+CommandHandlerInterface& OrderedPriorityListener::get_handler(std::string type) {
+    CommandHandlerInterface * local = nullptr;
+    m.lock();
+    if (handlers.count(type) > 0)
+        local = &handlers[type];
+    m.unlock();
+
+    if (local == nullptr)
+        throw std::invalid_argument("Listener has no set handler for type " + type);
+
+    return *local;
+}
+
+bool OrderedPriorityListener::command_available() {
+    m.lock();
+    auto local = command_queue.size() > 0;
+    m.unlock();
+
+    return local;
+}
+
+void OrderedPriorityListener::listener_loop() {
+    EnvironmentInterface& env = Client::get_instance().get_config().get_environment();
+
+    while (env.get_running()) {
+        if (command_available()) {
+            Command curr = poll_command();
+            try {
+                CommandHandlerInterface& handler = get_handler(curr.type);
+                handler.handle(curr.args);
+            } catch (std::exception& e) {
+                std::cout << "Warning: Listener was unable to process the following event: " << std::endl;
+                std::cout << "  type: " << curr.type << std::endl;
+                std::cout << "  args: " << std::endl;
+                std::cout << "```" << std::endl;
+                std::cout << curr.args << std::endl;
+                std::cout << "```" << std::endl;
+                std::cout << "raised exception " << e.what() << std::endl;
+            }
+        }
+    }
+}
+
+void OrderedPriorityListener::set_handler(std::string type, CommandHandlerInterface& handler) {
+    m.lock();
+    handlers[type] = handler;
+    m.unlock();
+}
+
+void OrderedPriorityListener::push_command(Command cmd) {
+    m.lock();
+    command_queue.push(cmd);
+    m.unlock();
+}
+
+void OrderedPriorityListener::run() {
+    std::thread t(&OrderedPriorityListener::run, this);
+}
