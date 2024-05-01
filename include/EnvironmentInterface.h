1162ac46325962f391403229a0d03f0c1cfd1a6b
Mason Hicks <mason@hicksm.dev>
2024-05-01 09:23:56 -0400

    Updated EnvironmentInterface with new variables #38

--- include/EnvironmentInterface.h
+++ include/EnvironmentInterface.h
@@ -15,19 +15,31 @@ namespace SPlat {
 
         virtual Model::Character * get_controlled_asset() = 0;
 
+        virtual size_t get_entrypoint_id() = 0;
+
         virtual std::set<sf::Keyboard::Key> get_held_keys() = 0;
+
+        virtual std::string get_pub_sub_addres() = 0;
+
+        virtual std::string get_req_rep_addres() = 0;
 
         virtual bool get_running() = 0;
 
-        virtual Model::StandingConfigInterface& get_standing_config() = 0;;
+        virtual Model::StandingConfigInterface& get_standing_config() = 0;
 
         virtual float get_unit() = 0;
 
         virtual void set_controlled_asset(Model::Character *) = 0;
 
+        virtual void set_entrypoint_id(size_t) = 0;
+
         virtual void add_held_key(sf::Keyboard::Key) = 0;
 
         virtual void remove_held_key(sf::Keyboard::Key) = 0;
+
+        virtual void set_pub_sub_addres(std::string) = 0;
+
+        virtual void set_req_rep_address(std::string) = 0;
 
         virtual void set_running(bool) = 0;
 
