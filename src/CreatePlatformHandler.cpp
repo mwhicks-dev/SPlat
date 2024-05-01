8cbb41821c7a6ca214a15c0f3f811a30b1a41a17
Mason Hicks <mason@hicksm.dev>
2024-05-01 14:27:59 -0400

    Added initial handlers #40

---
+++ src/CreatePlatformHandler.cpp
@@ -0,0 +1,62 @@
+#include "events/CreatePlatformHandler.h"
+#include "ControllerInterface.h"
+#include "Entrypoint.h"
+#include "Event.h"
+
+#include <cereal/archives/json.hpp>
+
+using namespace SPlat::Events;
+
+void CreatePlatformHandler::handle(std::string serialized) {
+    Entrypoint& entrypoint = Entrypoint::get_instance();
+    ConfigInterface& config = entrypoint.get_config();
+    EnvironmentInterface& environment = config.get_environment();
+    Timeline& anchor = config.get_timing_config().get_anchor_timeline();
+
+    // recreate event from details
+    SPlat::Event event = {
+        .event_time=anchor.get_time(), 
+        .command={
+            .priority=0,
+            .type=get_type(),
+            .args=serialized
+        },
+        .sender=environment.get_entrypoint_id()
+    };
+
+    // send to server as request
+    std::stringstream ss;
+    {
+        cereal::JSONOutputArchive oar(ss);
+        oar(event);
+    }
+    Request request = {
+        .content_type=Request::ContentType::Event,
+        .body=ss.str()
+    };
+    ControllerInterface& ctl = entrypoint.get_controller();
+    Response response = ctl.await(request);
+
+    if (response.status != 200) {
+        std::cerr << "Could not create platform(" << response.status << "):" << std::endl;
+        std::cerr << response.body << std::endl;
+        throw std::logic_error("");  // TODO create some TCPException class
+    }
+
+    SPlat::Event server_event;
+    {
+        std::stringstream iss; iss << response.body;
+        cereal::JSONInputArchive iar(iss);
+        iar(server_event);
+    }
+
+    Args args;
+    {
+        std::stringstream iss; iss << server_event.command.args;
+        cereal::JSONInputArchive iar(iss);
+        iar(args);
+    }
+
+    config.get_asset_factory_config().get_platform_factory().create_asset(args
+        .properties);
+}
---
+++ src/UpdateAssetHandler.cpp
@@ -0,0 +1,57 @@
+#include "events/UpdateAssetHandler.h"
+#include "ControllerInterface.h"
+#include "Entrypoint.h"
+#include "Event.h"
+
+#include <cereal/archives/json.hpp>
+
+using namespace SPlat::Events;
+
+void UpdateAssetHandler::handle(std::string serialized) {
+    Entrypoint& entrypoint = Entrypoint::get_instance();
+    ConfigInterface& config = entrypoint.get_config();
+    EnvironmentInterface& environment = config.get_environment();
+    Timeline& anchor = config.get_timing_config().get_anchor_timeline();
+
+    // recreate event from details
+    SPlat::Event event = {
+        .event_time=anchor.get_time(), 
+        .command={
+            .priority=0,
+            .type=get_type(),
+            .args=serialized
+        },
+        .sender=environment.get_entrypoint_id()
+    };
+
+    // send to server as request
+    std::stringstream ss;
+    {
+        cereal::JSONOutputArchive oar(ss);
+        oar(event);
+    }
+    Request request = {
+        .content_type=Request::ContentType::Event,
+        .body=ss.str()
+    };
+    ControllerInterface& ctl = entrypoint.get_controller();
+    Response response = ctl.await(request);
+
+    if (response.status != 200) {
+        std::cerr << "Could not update asset (" << response.status << "):" << std::endl;
+        std::cerr << response.body << std::endl;
+        throw std::logic_error("");  // TODO create some TCPException class
+    }
+
+    // perform update client-side
+    Args args;
+    {
+        std::stringstream ssi; ssi << serialized;
+        cereal::JSONInputArchive iar(ssi);
+        iar(args);
+    }
+    sf::Vector2f diff = entrypoint.get_object_model().read_asset(args.id)
+        .get_asset_properties().get_position() - args.properties
+        .get_position();
+    config.get_environment().get_standing_config().push_update_to_children(args.id, diff);
+}
