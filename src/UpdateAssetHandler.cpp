fe34cfcb7002b22754068bf9f6904c38f4361ea8
Mason Hicks <mason@hicksm.dev>
2024-05-02 08:38:58 -0400

    UpdateAssetHandler has separate flows depending on whether or not client already recognized object #55

--- src/UpdateAssetHandler.cpp
+++ src/UpdateAssetHandler.cpp
@@ -58,53 +58,62 @@ void UpdateAssetHandler::handle(std::string serialized) {
             .get_position();
         config.get_environment().get_standing_config().push_update_to_children(args.id, diff);
     } else {
-        // await GetAsset
-        GetAssetHandler::Args get_asset_args = {
-            .id=args.id
-        };
-        std::stringstream get_asset_args_ss;
-        {
-            cereal::JSONOutputArchive oar(get_asset_args_ss);
-            oar(get_asset_args);
-        }
-        Command get_asset_command = {
-            .priority=-2,
-            .type=GetAssetHandler::get_type(),
-            .args=get_asset_args_ss.str()
-        };
-        Event get_asset_event = {
-            .command=get_asset_command,
-            .sender=environment.get_entrypoint_id()
-        };
-        std::stringstream get_asset_event_ss;
-        {
-            cereal::JSONOutputArchive oar(get_asset_event_ss);
-            oar(get_asset_event);
-        }
-        Request get_asset_request = {
-            .content_type=Request::ContentType::Event,
-            .body=get_asset_event_ss.str()
-        };
-        Response get_asset_response = ctl.await(get_asset_request);
+        try {
+            // get asset from GOM
+            Model::Asset& asset = entrypoint.get_object_model().read_asset(args.id);
+            Model::AssetProperties& asset_properties = asset.get_asset_properties();
+            asset_properties.set_fill_color(args.properties.get_fill_color());
+            asset_properties.set_position(args.properties.get_position());
+            asset_properties.set_size(args.properties.get_size());
+        } catch (std::exception&) {
+            // retrieve asset from server if cannot find
+            GetAssetHandler::Args get_asset_args = {
+                .id=args.id
+            };
+            std::stringstream get_asset_args_ss;
+            {
+                cereal::JSONOutputArchive oar(get_asset_args_ss);
+                oar(get_asset_args);
+            }
+            Command get_asset_command = {
+                .priority=-2,
+                .type=GetAssetHandler::get_type(),
+                .args=get_asset_args_ss.str()
+            };
+            Event get_asset_event = {
+                .command=get_asset_command,
+                .sender=environment.get_entrypoint_id()
+            };
+            std::stringstream get_asset_event_ss;
+            {
+                cereal::JSONOutputArchive oar(get_asset_event_ss);
+                oar(get_asset_event);
+            }
+            Request get_asset_request = {
+                .content_type=Request::ContentType::Event,
+                .body=get_asset_event_ss.str()
+            };
+            Response get_asset_response = ctl.await(get_asset_request);
 
-        if (get_asset_response.status != 200) {
-            std::cerr << "Could not retrieve asset " << args.id << "(";
-            std::cerr << get_asset_response.status << "):" << std::endl;
-            std::cerr << get_asset_response.body << std::endl;
-            throw std::logic_error("");  // TODO create some TCPException class
-        }
+            if (get_asset_response.status != 200) {
+                std::cerr << "Could not retrieve asset " << args.id << "(";
+                std::cerr << get_asset_response.status << "):" << std::endl;
+                std::cerr << get_asset_response.body << std::endl;
+                throw std::logic_error("");  // TODO create some TCPException class
+            }
 
-        Model::AssetProperties asset_properties;
-        {
-            std::stringstream asset_properties_ss;
-            asset_properties_ss << get_asset_response.body;
-            cereal::JSONInputArchive iar(asset_properties_ss);
-            iar(asset_properties);
-        }
+            Model::AssetProperties asset_properties;
+            {
+                std::stringstream asset_properties_ss;
+                asset_properties_ss << get_asset_response.body;
+                cereal::JSONInputArchive iar(asset_properties_ss);
+                iar(asset_properties);
+            }
 
-        // TODO: find better check for asset type than assuming they're a platform
-        config.get_asset_factory_config().get_platform_factory()
-            .create_asset(asset_properties);
+            // TODO: find better check for asset type than assuming they're a platform
+            config.get_asset_factory_config().get_platform_factory()
+                .create_asset(asset_properties);
+        }
     }
     
 #ifdef DEBUG
