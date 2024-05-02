1b5f38f9fcb80a352584762deed7fc9d4d57c183
Mason Hicks <mason@hicksm.dev>
2024-05-01 21:14:28 -0400

    Removed event_time/client_side from Event + relationals use command priority #48

--- include/Event.h
+++ include/Event.h
@@ -8,32 +8,28 @@ namespace SPlat {
     /// @brief event state
     struct Event {
 
-        time_t event_time = 0;
-
         Events::Command command;
-
-        bool client_side = true;
 
         size_t sender = 0;
 
         template <class Archive>
         void serialize(Archive& ar) {
-            ar(event_time, command, client_side, sender);
+            ar(command, sender);
         }
         
         /// @brief overload < operator 
         friend bool operator<(const Event& lhs, const Event& rhs) {
-            return lhs.event_time < rhs.event_time;
+            return lhs.command.priority < rhs.command.priority;
         }
 
         /// @brief overload > operator 
         friend bool operator>(const Event& lhs, const Event& rhs) {
-            return lhs.event_time > rhs.event_time;
+            return lhs.command.priority > rhs.command.priority;
         }
 
         /// @brief overload == operator 
         friend bool operator==(const Event& lhs, const Event& rhs) {
-            return lhs.event_time == rhs.event_time;
+            return lhs.command.priority == rhs.command.priority;
         }
 
         /// @brief overload != operator 
